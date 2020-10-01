59254c7b78cd4856eee094b2a537bec91fb835fb
HermBB <39308818+HermBB@users.noreply.github.com>
2020-10-01 11:31:18 -0400

    Debut du Virage

---
+++ test_hbb/src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  //pinMode(LED_BUILTIN,OUTPUT);
+  BoardInit();
+  AudioInit();
+  DisplayInit();
+  BluetoothInit();
+
+ Serial.println("Hello world");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Flasher LED
+  /*
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  */
+ MOTOR_SetSpeed(0,-1);
+ MOTOR_SetSpeed(1,-1);
+ delay(3000);
+ MOTOR_SetSpeed(0,0);
+ MOTOR_SetSpeed(1,0);
+int32_t encodeur_G = ENCODER_Read(0);
+int32_t encodeur_D = ENCODER_Read(1);
+char encodeur_value[64] = {0};
+
+sprintf(encodeur_value, "%ld", encodeur_D);
+Serial.println(encodeur_value);
+
+sprintf(encodeur_value, "%ld", encodeur_G);
+Serial.println(encodeur_value);
+
+while (true)
+{
+  /* code */
+}
+
+
+}
